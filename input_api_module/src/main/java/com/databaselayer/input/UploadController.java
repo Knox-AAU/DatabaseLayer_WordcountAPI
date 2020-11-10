a6caa5548af836d23ab1b0068522cfb3c5630a8c
djcooland <larsdjand@gmail.com>
2020-11-10 13:40:15 +0100

    InputApi basic test of update

--- input_api_module/src/main/java/com/databaselayer/input/UploadController.java
+++ input_api_module/src/main/java/com/databaselayer/input/UploadController.java
@@ -1,8 +1,14 @@
 package com.databaselayer.input;
-import com.hp.hpl.jena.query.*;
+
+import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFFormat;
+import org.apache.jena.update.UpdateExecutionFactory;
+import org.apache.jena.update.UpdateFactory;
+import org.apache.jena.update.UpdateProcessor;
+import org.apache.jena.update.UpdateRequest;
 import org.apache.jena.util.FileManager;
 import org.apache.jena.util.FileManagerImpl;
 import org.rdfhdt.hdt.enums.RDFNotation;
@@ -19,22 +25,33 @@ import java.io.*;
 @SpringBootApplication
 @RestController
 public class UploadController {
-
+    static final String HDTDataPath = "/etc/fuseki/databases/hdt/database.hdt";
+    static final String RDFDataPath = "/home/databaseInputApi/temp.nt";
+    static final String RDFOutput =    "/home/databaseInputApi/output.nt";
+    //Måske husk at slette index, når filen er blevet erstattet /todo
     // Takes a string (.ttl) and recompresses the HDT with the new information and restarts the server.
     @PostMapping("/update")
-    public String uploadFile(@RequestBody String file) throws FileNotFoundException {
+    public String uploadFile(@RequestBody String inputTriples) throws IOException {
         // Decompress HDT
 
-
+        Process p = Runtime.getRuntime().exec("java -server -Xmx1024M -classpath 'lib/*'" +
+                " org.rdfhdt.hdt.tools.HDT2RDF "+ HDTDataPath +  " " + RDFDataPath);
         // QueryBuilder to access Decompressed HDT
-        InputStream inputStream = new FileInputStream(
-                new File("/etc/fuseki/data.ttl"));
+        Model model = ModelFactory.createDefaultModel();
+        model.read(new FileInputStream(RDFDataPath),null,"N-TRIPLES");
+        org.apache.jena.query.Dataset dataset = DatasetFactory.assemble(model);
+        UpdateRequest updateQuery = UpdateFactory.create("INSERT DATA { " + inputTriples + " }");
+        UpdateProcessor updater = UpdateExecutionFactory.create(updateQuery, dataset);
 
-        // Model model = FileManagerImpl.loadModelInternal("/etc/fuseki/data.ttl");
-        // Model newModel = ModelFactory.createDefaultModel() ;
-        
+
+        // Query updateQuery = QueryFactory.create("INSERT DATA { " + inputTriples + " }" );
+        // QueryExecution queryExecutor = QueryExecutionFactory.create(updateQuery, model);
+        try {
+            updater.execute();
+        }
+        catch (Exception e){e.printStackTrace();}
         // Save QueryBuilder as RDF
-
+        RDFDataMgr.write(new FileOutputStream(RDFOutput), dataset, RDFFormat.TTL );
 
         return "Updated";
     }
