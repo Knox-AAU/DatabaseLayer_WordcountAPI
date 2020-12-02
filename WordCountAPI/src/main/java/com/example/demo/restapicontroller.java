f7dc67471831785a4cc4a2478074621bb8491f2f
dmorra18 <dmorra18@student.aau.dk>
2020-12-02 11:15:46 +0100

    updated controller to handle arrays and updated exceptions

--- WordCountAPI/src/main/java/com/example/demo/restapicontroller.java
+++ WordCountAPI/src/main/java/com/example/demo/restapicontroller.java
@@ -1,4 +1,5 @@
 package com.example.demo;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -23,67 +24,96 @@ import javax.sql.*;
 public class restapicontroller {
     @Autowired
     JdbcTemplate jdbc;
+
     @PostMapping(value = "/wordCountData", consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public String insert(@RequestBody String input) throws ParseException, SQLException {
-        String word, filepath, articletitle;
+    public String insert(@RequestBody String input) throws Exception {
         List<Map<String, Object>> rows = jdbc.queryForList("SELECT * FROM wordlist");
         //System.out.println(rows);
         JSONParser parser = new JSONParser();
         JSONObject object = (JSONObject) parser.parse(input);
+
+
+
+//            JSONObject jsonlist = (JSONObject) object.get("words");
+        JSONArray jsonarray = (JSONArray) object.get("words");
+        String articletitle = (String) object.get("articletitle");
+        String filepath = (String) object.get("filepath");
+
+        if(articletitle == null)
+        {
+            throw new Exception("Article title must not be null");
+        }
+        if(filepath == null)
+        {
+            throw new Exception("Filepath must not be null");
+        }
+
+
+            insertfiledata("filelist(filepath, articletitle, totalwordsinarticle)",
+                    (String) object.get("filepath"),
+                    (String) object.get("articletitle"),
+                    Integer.parseInt(object.get("totalwordsinarticle").toString()));
+
+
+
+        for (int i = 0; i < jsonarray.size(); i++) {
+            JSONObject jsonEntry = (JSONObject) jsonarray.get(i);
+            String entryWord = (String) jsonEntry.get("word");
+            if(entryWord == null)
+            {
+                throw new Exception("word must not be null");
+            }
+
+            long entryAmount = (long) jsonEntry.get("amount");
+            System.out.println(entryWord + " " + entryAmount);
+            insertworddata("wordlist(wordname)", entryWord);
+
+            insertappearsdata("appearsin(amount, filepath, articletitle, wordname)",
+                    (int) entryAmount, (String) object.get("filepath"), (String) object.get("articletitle"), entryWord);
+        }
+
+
+
+
         //word = (String) object.get("word");
         System.out.println(" word: " + object.get("word") + '\n'
                 + " filepath: " + object.get("filepath") + '\n'
                 + " articletitle: " + object.get("articletitle") + '\n'
                 + " totalwordsinarticle: " + object.get("totalwordsinarticle"));
-        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/wordcount", "postgres", "1234");
-        if(object.get("word") != null);
-        {
-            insertworddata(connection, "wordlist(wordname)", (String) object.get("word"));
-        }
-        if(object.get("filepath") != null && object.get("articletitle") != null && object.get("totalwordsinarticle") != null);
-        {
-            insertfiledata(connection,"filelist(filepath, articletitle, totalwordsinarticle)",
-                    (String) object.get("filepath"),
-                    (String) object.get("articletitle"),
-                    Integer.parseInt(object.get("totalwordsinarticle").toString()));
-        }
-        if(object.get("amount") != null && object.get("wordname") != null && object.get("filepath") != null && object.get("articletitle") != null)
+
+
+
+
+        /*
+        if(object.get("amount") != null && object.get("word") != null && object.get("filepath") != null && object.get("articletitle") != null)
         {
             //insert data; amount, wordname, filepath, articletitle
-            insertappearsdata(connection, "appearsin(amount, filepath, articletitle, wordname))",
+            insertappearsdata("appearsin(amount, filepath, articletitle, wordname)",
                     Integer.parseInt(object.get("amount").toString()),
                     (String) object.get("filepath"),
                     (String) object.get("articletitle"),
-                    (String) object.get("wordname")
+                    (String) object.get("word")
             );
-        }
-        if (false) {
-            throw new IllegalArgumentException(" Invalid input");
-        }
-        else {
-            return "Values inserted";
-        }
+        } */
+        return "done";
     }
-    public int insertfiledata(Connection connection, String tableref, String datavalue1,
-                              String datavalue2, int datavalue3) throws SQLException {
-        String sql = String.format("INSERT INTO %s" + "VALUES ('%s','%s',%d)", tableref,datavalue1, datavalue2, datavalue3);
-        Statement stmt = null;
-        stmt = connection.createStatement();
+    public int insertfiledata(String tableref, String filepath,
+                              String articletitle, int totalwordsinarticle) throws SQLException{
+        String sql = String.format("INSERT INTO %s" + "VALUES ('%s','%s',%d) ON CONFLICT DO NOTHING",
+                tableref,filepath, articletitle, totalwordsinarticle);
         jdbc.update(sql);
         return 1;
     }
-    public int insertworddata(Connection connection, String tableref, String word) throws SQLException {
+
+    public int insertworddata(String tableref, String word) throws SQLException {
         String sql = String.format("INSERT INTO %s " + "VALUES ('%s') ON CONFLICT DO NOTHING",tableref, word);
-        Statement stmt = null;
-        stmt = connection.createStatement();
         jdbc.update(sql);
         return 1;
     }
-    public int insertappearsdata(Connection connection, String tableref, int amount,
+
+    public int insertappearsdata(String tableref, int amount,
                                  String filepath, String filename, String word) throws SQLException {
-        String sql = String.format("INSERT INTO %s" + "VALUES (%d, '%s', '%s', '%s')", tableref, amount, filepath, filename, word);
-        Statement stmt = null;
-        stmt = connection.createStatement();
+        String sql = String.format("INSERT INTO %s" + "VALUES (%d, '%s', '%s', '%s') ON CONFLICT DO NOTHING", tableref, amount, filepath, filename, word);
         jdbc.update(sql);
         return 1;
     }
