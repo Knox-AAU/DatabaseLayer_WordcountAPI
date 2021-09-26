b2f78f1274908785a2803ca81ae476f3e543eb4c
Rasmus Høyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-09-26 14:50:42 +0200

    its a start

---
+++ WordCount/API/Controllers/WordCountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KnoxDatabaseLayer3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WordCountController : Controller
+    {
+        // GET
+        [HttpGet]
+        public void Get([FromBody] TurtleJson t)
+        {
+                //Steps: try to convert to json, on fail, log the exception
+                    //Create file log class/service
+                //insert file data into 'data layer' class
+                //save data
+        }
+    }
+
+}
