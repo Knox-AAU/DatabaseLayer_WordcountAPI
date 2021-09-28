98da213dda9d78f83ca83ce45150012519ed779d
Pattrigue <patrick@gamehouse.dk>
2021-09-28 22:13:30 +0200

    Update JsonValidatorTests.cs

--- WordCount/WordCountUnitTests/JsonValidatorTests.cs
+++ WordCount/WordCountUnitTests/JsonValidatorTests.cs
@@ -1,79 +1,70 @@
-using System;
 using NUnit.Framework;
 using WordCount.JsonModels;
 
 namespace WordCountUnitTests
 {
-    public class JsonValidatorTests
+    public sealed class JsonValidatorTests
     {
-        [SetUp]
-        public void Setup()
-        {
-            Console.WriteLine();
-        }
-
-        [Test]
-        public void TestJsonObject()
-        {
-            const string schema = @"{
-              ""type"": ""object"",
-              ""properties"": {
-                ""name"": {""type"": ""string""},
-                ""roles"": {""type"": ""array""}
-              }
-            }";
-
-            const string json = @"{
-              ""name"": ""Arnie Admin"",
-              ""roles"": [""Developer"", ""Administrator""]
-            }";
-            
-            Assert.True(new JsonValidator<UserModel>(schema).IsObjectValid(json, out UserModel userModel));
-            
-            Assert.That(userModel.Name == "Arnie Admin");
-            Assert.That(userModel.Roles.Length == 2);
-            Assert.That(userModel.Roles[0] == "Developer");
-            Assert.That(userModel.Roles[1] == "Administrator");
-        }
-        
         private sealed class UserModel
         {
             public string Name { get; set; }
             public string[] Roles { get; set; } 
         }
         
+        private const string JsonObjectSchema = @"{
+            ""type"": ""object"",
+            ""properties"": {
+                ""name"": {""type"": ""string""},
+                ""roles"": {""type"": ""array""}
+            }
+        }";
+
+        private const string JsonObject = @"{
+            ""name"": ""Arnie Admin"",
+            ""roles"": [""Developer"", ""Administrator""]
+        }";
+        
+        private const string JsonArarySchema = @"{
+            ""type"": ""array"",
+            ""items"": {
+                ""type"": ""object"",
+                ""properties"": {
+                    ""name"": {""type"": ""string""},
+                    ""roles"": {""type"": ""array""},
+                }
+            }
+        }";
+
+        private const string JsonArray = @"[
+            {
+                ""name"": ""Jason"",
+                ""roles"": [""Developer"", ""Administrator""]
+            },
+            {
+                ""name"": ""Jack"",
+                ""roles"": [""Manager""]
+            }
+        ]";
+
+        [Test]
+        public void TestJsonObject()
+        {
+            Assert.True(new JsonValidator<UserModel>(JsonObjectSchema).IsObjectValid(JsonObject, out UserModel userModel));
+            
+            Assert.That(userModel.Name == "Arnie Admin");
+            Assert.That(userModel.Roles.Length == 2);
+            Assert.That(userModel.Roles[0] == "Developer");
+            Assert.That(userModel.Roles[1] == "Administrator");
+        }
+      
         [Test]
         public void TestJsonArray()
         {
-            const string schema = @"{
-              ""type"": ""array"",
-              ""items"": {
-                ""type"": ""object"",
-                ""properties"": {
-                  ""name"": {""type"": ""string""}
-                }
-              }
-            }";
-
-            const string json = @"[
-              {
-                ""name"": ""Jason""
-              },
-              {
-                ""name"": ""Jack""
-              }
-            ]";
-            
-            Assert.True(new JsonValidator<NameModel[]>(schema).IsArrayValid(json, out NameModel[] nameModels));
+            Assert.True(new JsonValidator<UserModel[]>(JsonArarySchema).IsArrayValid(JsonArray, out UserModel[] nameModels));
             
             Assert.That(nameModels.Length == 2);
             Assert.That(nameModels[0].Name == "Jason");
             Assert.That(nameModels[1].Name == "Jack");
-        }
- 
-        private sealed class NameModel
-        {
-            public string Name { get; set; }
         }
     }
 }
