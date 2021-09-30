2f64776f6e0d4ba0a6aaf4de0bb0474e2509aca5
Pattrigue <pfas19@student.aau.dk>
2021-09-30 10:49:57 +0200

    Working connection string

--- WordCount/Startup.cs
+++ WordCount/Startup.cs
@@ -16,15 +16,6 @@ namespace WordCount
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-            var x = configuration.GetValue<string>("WordCountDb");
-            Console.WriteLine(x);
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
