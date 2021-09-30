6e3f03b324d454f4f6a12e21d7ddee46b8260502
Pattrigue <pfas19@student.aau.dk>
2021-09-30 10:49:57 +0200

    Working connection string

--- WordCount/Program.cs
+++ WordCount/Program.cs
@@ -14,7 +14,6 @@ namespace WordCount
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
