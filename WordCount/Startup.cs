907a261d8b412104fac860059c31b692da209e20
Ivikhostrup <56341364+Ivikhostrup@users.noreply.github.com>
2021-09-30 09:42:19 +0200

    Update Startup.cs

--- WordCount/Startup.cs
+++ WordCount/Startup.cs
@@ -1,3 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
 namespace WordCount
 {
     public class Startup
@@ -6,7 +20,7 @@ namespace WordCount
         {
             Configuration = configuration;
             var x = configuration.GetValue<string>("WordCountDb");
-            global::System.Console.WriteLine(x);
+            Console.WriteLine(x);
         }
 
         public IConfiguration Configuration { get; }
