e6c1101764f68bf0a490c2f5fba732290280693a
Christian Bager Bach Houmann <chhoumann@gmail.com>
2021-10-01 07:39:46 +0200

    Docker progress.

--- WordCount/WordCountAPI/Controllers/WeatherForecastController.cs
+++ WordCount/WordCountAPI/Controllers/WeatherForecastController.cs
@@ -24,16 +24,15 @@ namespace WordCount.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
-                })
-                .ToArray();
+            string environmentVariables = "";
+            foreach (var key in Environment.GetEnvironmentVariables().Keys)
+            {
+                environmentVariables += $"{key}:{Environment.GetEnvironmentVariable(key.ToString())}\n";
+            }
+
+            return environmentVariables;
         }
     }
 }
--- WordCount/WordCountAPI/Startup.cs
+++ WordCount/WordCountAPI/Startup.cs
@@ -41,9 +41,8 @@ namespace WordCount
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WordCount v1"));
+                app.UseHttpsRedirection();
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
