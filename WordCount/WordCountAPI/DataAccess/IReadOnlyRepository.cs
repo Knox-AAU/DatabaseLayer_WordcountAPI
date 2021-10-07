819f32cf84d354ebba3363fd5c90e14f200b47c5
Rasmus Høyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-10-07 13:17:26 +0200

    Readonly repository and belonging interface
    Created a readonly repository where no tracking of the DbContext is used, thus saving is not possible.
    The repository workd on queries and does not convert to list.

---
+++ WordCount/WordCountAPI/DataAccess/IReadOnlyRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordCount.DataAccess
+{
+    public interface IReadOnlyRepository<TEntity>
+    {
+        public TEntity GetById(int id);
+        public IEnumerable<TEntity> All();
+        public TEntity Find(Predicate<TEntity> predicate);
+        public IEnumerable<TEntity> FindAll(Predicate<TEntity> predicate);
+
+        public IEnumerable<TEntity> Get (
+            Predicate<TEntity> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null);
+    }
+}
---
+++ WordCount/WordCountAPI/DataAccess/ReadOnlyRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Data.Entity;
+
+namespace WordCount.DataAccess
+{
+    public sealed class ReadOnlyRepository<T> : IReadOnlyRepository<T> where T : EntityModel
+    {
+        private readonly IQueryable<T> _entities;
+
+        public ReadOnlyRepository(DbContext context)
+        {
+            _entities = context.Set<T>().AsNoTracking();
+        }
+
+        /// <summary>
+        /// Find first entity with matching id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public T GetById(int id)
+        {
+            return _entities.First(e => e.Id == id);
+        }
+
+        public IEnumerable<T> All()
+        {
+            return _entities;
+        }
+
+        public T Find(Predicate<T> predicate)
+        {
+            return _entities.First(e => predicate(e));
+        }
+
+        public IEnumerable<T> FindAll(Predicate<T> predicate)
+        {
+            return _entities.Where(e => predicate(e));
+        }
+
+        public IEnumerable<T> Get(Predicate<T> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null)
+        {
+            IQueryable<T> query = _entities;
+            if (filter != null)
+            {
+                query = query.Where(e => filter(e));
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query);
+            }
+
+            return query;
+        }
+    }
+}
