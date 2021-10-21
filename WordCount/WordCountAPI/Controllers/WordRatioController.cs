8563a8f343414007258c3e9211e6a5d7f238602f
Rasmus Høyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-10-21 10:14:15 +0200

    basic setup

--- WordCount/WordCountAPI/Controllers/WordRatioController.cs
+++ WordCount/WordCountAPI/Controllers/WordRatioController.cs
@@ -14,5 +14,25 @@ namespace WordCount.Controllers
         {
             return new WordCountDbContext().WordRatios.ToList();
         }
+        
+        [HttpGet]
+        [Route("/[controller]/")]
+        public IEnumerable<WordRatios> GetMatches(string[] terms, string[] sources)
+        { 
+            if (terms == null || terms.Length == 0)
+            {
+                //TODO Error message    
+                return null;
+            }
+            
+            List<WordRatios> set = new WordCountDbContext().WordRatios.ToList();
+            IEnumerable<WordRatios> result = set.Where(w => terms.Contains(w.WordName));
+            
+            if (sources != null && sources.Length != 0)
+                result = result.Where(r => sources.Contains(r.SourceName));
+
+
+            return result.OrderBy(a => a.ArticleTitle);
+        }
     }
 }
