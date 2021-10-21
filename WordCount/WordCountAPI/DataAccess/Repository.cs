48b07d73f80c1921a5377c3493bf64120849ac94
Rasmus Høyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-10-21 18:08:54 +0200

    Subscribed to EventList from base class. We now have a repository that saves when relevant

--- WordCount/WordCountAPI/DataAccess/Repository.cs
+++ WordCount/WordCountAPI/DataAccess/Repository.cs
@@ -11,51 +11,61 @@ namespace WordCount.DataAccess
         where TKey : IEquatable<TKey>
     {
         private readonly DbContext context;
-        public event Action ElementsSaved;
-        public event Action ElementsRemoved;
-        public event Action ElementsUpdated;
 
         public Repository(DbContext context) : base(context.Set<TEntity>())
         {
             this.context = context;
-            ListChanged += Save;
+            InternalEntitySet.ItemAdded += Add;
+            InternalEntitySet.ItemsAdded += Add;
+            InternalEntitySet.ItemRemoved += Remove;
+            InternalEntitySet.ItemsRemoved += Remove;
         }
+
+        private void Add(IEnumerable<TEntity> obj)
+        {
+            context.AddRange(obj);
+            Save();
+        }
+
+        private void Add(TEntity obj)
+        {
+            context.Add(obj);
+            Save();
+        }
+        
 
         /// <summary>
         /// Saves the elements in the DbSet to the database.
         /// </summary>
         private void Save()
         {
-            context.AddRange(InternalEntitySet);
-            ElementsSaved?.Invoke();
+            context.SaveChanges();
         }
 
         public void Update(TEntity entity)
         {
             context.Update(entity);
-            ElementsUpdated?.Invoke();
             Save();
         }
         
         public void Update(IEnumerable<TEntity> entities)
         {
             context.UpdateRange(entities);
-            ElementsUpdated?.Invoke();
             Save();
         }
         
         public void Remove(TEntity entities)
         {
             context.Remove(entities);
-            ElementsRemoved?.Invoke();
             Save();
         }
         public void Remove(IEnumerable<TEntity> entities)
         {
             context.Remove(entities);
-            ElementsRemoved?.Invoke();
             Save();
         }
+        
+        
         
     }
 }
