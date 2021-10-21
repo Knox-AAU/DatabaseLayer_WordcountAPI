d11c71d32b13a7ed3aa63cd84b824a82c7dcd628
Pattrigue <pfas19@student.aau.dk>
2021-10-21 17:19:06 +0200

    Create EventList.cs

---
+++ WordCount/WordCountAPI/DataAccess/EventList.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WordCount.DataAccess
+{
+    public sealed class EventList<T> : IList<T>, IReadOnlyList<T>
+    {
+        public event Action<T> ItemAdded;
+        public event Action<int, T> ItemInserted;
+        public event Action<T> ItemRemoved;
+        public event Action ListCleared;
+
+        public bool IsReadOnly => false;
+        
+        int ICollection<T>.Count => internalList.Count;
+
+        int IReadOnlyCollection<T>.Count => internalList.Count;
+        
+        private readonly List<T> internalList = new();
+
+        public void Add(T item)
+        {
+            internalList.Add(item);
+            ItemAdded?.Invoke(item);
+        }
+
+        public void Clear()
+        {
+            internalList.Clear();
+            ListCleared?.Invoke();
+        }
+
+        public bool Contains(T item) => internalList.Contains(item);
+
+        public void CopyTo(T[] array, int arrayIndex) => internalList.CopyTo(array, arrayIndex);
+
+        bool ICollection<T>.Remove(T item)
+        {
+            if (!internalList.Remove(item)) return false;
+            
+            ItemRemoved?.Invoke(item);
+            return true;
+        }
+        
+        public void Remove(T item)
+        {
+            internalList.Remove(item);
+            ItemRemoved?.Invoke(item);
+        }
+
+        public IEnumerator<T> GetEnumerator() => internalList.GetEnumerator();
+        
+        IEnumerator IEnumerable.GetEnumerator() => internalList.GetEnumerator();
+
+        public int IndexOf(T item) => internalList.IndexOf(item);
+
+        public void Insert(int index, T item)
+        {
+            internalList.Insert(index, item);
+            ItemInserted?.Invoke(index, item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            T item = this[index];
+            
+            internalList.RemoveAt(index);
+            ItemRemoved?.Invoke(item);
+        }
+
+        public T this[int index]
+        {
+            get => internalList[index];
+            set => internalList[index] = value;
+        }
+    }
+}
