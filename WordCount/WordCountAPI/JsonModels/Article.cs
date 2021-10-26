d1d9c3af1a3b4a51f1f61d0e0ffee871d722b753
Rasmus Høyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-10-26 10:13:51 +0200

    added publication field

--- WordCount/WordCountAPI/JsonModels/Article.cs
+++ WordCount/WordCountAPI/JsonModels/Article.cs
@@ -7,6 +7,7 @@ namespace WordCount.JsonModels
     public sealed class Article
     {
         public string ArticleTitle { get; set; }
+        public string Publication { get; set; }
         public string FilePath { get; set; }
         public int TotalWordsInArticle { get; set; }
         public WordData[] Words { get; set; }
