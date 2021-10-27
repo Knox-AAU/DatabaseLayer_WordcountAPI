22220eab4a0ee15174ba5b12cd355f24f685021e
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-10-27 21:29:25 +0200

    Namespace and return type change

--- WordCount/WordCountUnitTests/ReadOnlyRepositoryTest.cs
+++ WordCount/WordCountUnitTests/ReadOnlyRepositoryTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
+using WordCount.Data.DataAccess;
 using WordCount.DataAccess;
 
 namespace WordCountUnitTests
