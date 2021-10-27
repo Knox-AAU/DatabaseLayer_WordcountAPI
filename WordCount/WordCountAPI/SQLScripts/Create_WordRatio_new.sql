3f1b06cacbffeeb9920f775d6f7f276e10a4cdab
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-10-27 18:23:14 +0200

    new wordratio

---
+++ WordCount/WordCountAPI/SQLScripts/Create_WordRatio_new.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW WordRatio AS
+SELECT "ArticleId", "Word", "Count", "Title", "FilePath", "TotalWords","PublisherId", "PublisherName",
+       round("Count"::numeric / "TotalWords"::numeric * 100::numeric, 2) AS Percent
+
+FROM "Article"
+         JOIN "Term" ON "ArticleId" = "Article"."Id"
+         JOIN "Publisher" ON "Article"."PublisherId" = "PublisherId"
