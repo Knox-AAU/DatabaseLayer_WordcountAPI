a5a39d18f8e7a8107a03f1f8bcb48b48d9752b12
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2021-11-30 20:31:01 +0100

    Updated algorithm so 50 articles will be examined and added until no more are left

--- WordCount/WordCountAPI/Controllers/OldModelToNewModelController.cs
+++ WordCount/WordCountAPI/Controllers/OldModelToNewModelController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -26,49 +27,62 @@ namespace WordCount.Controllers
         public IActionResult Migrate(string pw)
         {
             if (pw != "please_do_migration")
+            {
                 return BadRequest();
+            }
 
             var oldArticles = OldContext.Articles;
             DbSet<Publisher_old> oldPublishers = OldContext.Publishers;
 
             var newArticles = NewContext.Articles;
             var newPublishers = NewContext.Publishers;
-
             oldArticles.AsQueryable();
 
-            foreach (var publisher in oldPublishers)
+            foreach (var oldPublisher in oldPublishers)
             {
-                // The publisher is a root of the data base structure. It is not possible to have duplicated,
-                // thus no need to check first.
-                Publisher createdPublisher = new() { PublisherName = publisher.PublisherName };
-                IEnumerable<Article_old> firstArticles = publisher.Articles.Take(50);
-                
-                // build entire structure, then add to root structure.
-                AddArticles(firstArticles, createdPublisher);
-                newPublishers.Add(createdPublisher);
-                NewContext.SaveChanges();
+                int addedArticles = 0;
+                IEnumerable<Article_old> articlesToAggregate;
+                do
+                {
+                    // Create root publisher
+                    Publisher newPublisher = new Publisher() { PublisherName = oldPublisher.PublisherName };
+                    articlesToAggregate = oldPublisher.Articles.Skip(addedArticles).Take(50);
+
+                    // Create and attach/aggregate articles to root.
+                    AggregateArticles(articlesToAggregate, newPublisher);
+
+                    // Save the first 50
+                    NewContext.SaveChanges();
+                    addedArticles += 50;
+                }
+                while (articlesToAggregate.Any()); // if any articles are left
             }
 
             return null;
         }
 
-        private void AddArticles(IEnumerable<Article_old> firstArticles, Publisher publisher)
+        private void AggregateArticles(IEnumerable<Article_old> firstArticles,  Publisher newPublisher)
         {
             foreach (Article_old oldArticle in firstArticles)
             {
+                // The publisher is a root of the data base structure.
+                // It is not possible to have duplicates of it, thus no need to check in DB first.
                 // Publishers have guaranteed unique articles, therefore no need to check if articles exist
                 // in new database.
                 var createdArticle = new Article()
                 {
-                    Id = oldArticle.Id, TotalWords = oldArticle.TotalWords,
-                    FilePath = oldArticle.FilePath, Title = oldArticle.Title, Publisher = publisher
+                    Id = oldArticle.Id,
+                    TotalWords = oldArticle.TotalWords,
+                    FilePath = oldArticle.FilePath,
+                    Title = oldArticle.Title,
+                    Publisher = newPublisher
                 };
 
-                AddWordRatio(createdArticle, oldArticle.Terms);
+                AggregateWordOccurance(createdArticle, oldArticle.Terms);
             }
         }
 
-        private void AddWordRatio(Article createdArticle, List<Term> oldArticleTerms)
+        private void AggregateWordOccurance(Article createdArticle, List<Term> oldArticleTerms)
         {
             foreach (Term term in oldArticleTerms)
             {
